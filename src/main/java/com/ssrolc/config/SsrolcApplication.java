854d902adb49df378cd81a241ffd110b89380abd
mashisoon <mashisoon@jeione.com>
2015-07-31 13:15:49 +0900

    jndi설정 으잉ㅠㅠ

--- src/main/java/com/ssrolc/config/SsrolcApplication.java
+++ src/main/java/com/ssrolc/config/SsrolcApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SsrolcApplication {
+public class SsrolcApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SsrolcApplication.class, args);
