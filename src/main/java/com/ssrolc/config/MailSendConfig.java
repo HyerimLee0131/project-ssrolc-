372b3f93f7010ee2bb3525eda47092b14b8ddf0e
mashisoon <mashisoon@jeione.com>
2015-08-18 16:12:04 +0900

    2121212121

--- src/main/java/com/ssrolc/config/MailSendConfig.java
+++ src/main/java/com/ssrolc/config/MailSendConfig.java
@@ -2,16 +2,21 @@ package com.ssrolc.config;
 
 import lombok.Data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import com.ssrolc.controller.ssrolcmanager.BoardController;
+
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "mail")
 public class MailSendConfig {
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	
 	private String host;
 	private int port;
@@ -21,7 +26,7 @@ public class MailSendConfig {
 	@Bean
 	public JavaMailSender mailSender(){
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		System.out.println(username+"==========================");
+		logger.debug(username+"==========================");
 		mailSender.setHost(host);
 		mailSender.setUsername(username);
 		mailSender.setPassword(password);
